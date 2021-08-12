36e65d9b20979f5600d4534077ed98ae0c50b29e
Harish Akkala <harish.akkala@rallyhealth.com>
2021-08-12 14:55:46 +0530

    ORCA-725-Hanler code

--- app/controllers/PlayTestController.scala
+++ app/controllers/PlayTestController.scala
@@ -1,80 +1,46 @@
 package controllers
 
-import java.time.Clock
-
 import Actions.UserAction
+import com.rallyhealth.rq.v1.handler.HandledRqRequest
 import javax.inject._
 import play.api.Configuration
-import play.api.http.HttpVerbs
 import play.api.libs.json.Json
 import play.api.mvc._
 import utils.LegacyCookieGenerator
-import com.rallyhealth.rq.v1.handler.{ExecutableRqRequest, HandledRqRequest}
 import com.rallyhealth.rq.v1.handler.Predicate.{OkOrNotFound, PredicateHandler}
-import com.rallyhealth.rq.v1.handler.RqResponseHandlers.ResponseHandler
-import com.rallyhealth.rq.v1.handler.Status.{NoContent, Ok}
-import com.rallyhealth.rq.v1._
-
-import scala.concurrent.{ExecutionContext, Future}
-import com.rallyhealth.rq.v1._
-import com.rallyhealth.rq.v1.handler.ExecutableRqRequest
-import com.rallyhealth.rq.v1.handler.Predicate.OkOrNotFound
-import com.softwaremill.tagging.@@
 import play.api.http.HttpVerbs
 
-
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class PlayTestController @Inject()(cc: ControllerComponents, config: Configuration, userAction: UserAction) extends AbstractController(cc) {
-
-//  private implicit val unitPredicateHandler: PredicateHandler[String] = (x: RqResponse) => x.body.toString
-
 
   def getPcps = userAction {
     request => {
       Ok(Json.toJson(new CareteamClient(new CareteamRequestBuilder()
     }
   }
-
-//  def getPlayResult(
-//                       rallyId: String
-//                     ): Option[String] =
-//    Rq.url("http://localhost:8018/rest/careteam/v1/primaryCare/" + rallyId)
-//      .withHeaders(LegacyCookieGenerator.encodeSessionCookie(rallyId).toSeq: _*)
-//      .withMethod(HttpVerbs.GET)
-//      .withHandler(OkOrNotFound[String])
-//      .execute
-
-//  def getCareteamPcps(
-//       rallyId: String
-//  ): Option[FamilyPrimaryCare] =
-//    Rq.url(config.baseUrl + "/rest/careteam/v1/primaryCare/" + rallyId)
-//      .withHeaders(CookieShim.encodeSessionCookie(rallyId).toSeq: _*)
-//      .withMethod(HttpVerbs.GET)
-//      .withHandler(OkOrNotFound[FamilyPrimaryCare])
-//      .withRqClient(careteamRqClient)
-//      .execute
 }
 
 
 import com.rallyhealth.rq.v1._
-import com.rallyhealth.rq.v1.handler.ExecutableRqRequest
-import com.rallyhealth.rq.v1.handler.Predicate.OkOrNotFound
-import com.softwaremill.tagging.@@
-import play.api.http.HttpVerbs
 
-class CareteamClient(
-  careteamRequestBuilder: CareteamRequestBuilder
-)(implicit
+class CareteamClient()(implicit
   ec: ExecutionContext
 ) {
 
+  val careteamRequestBuilder: CareteamRequestBuilder = new CareteamRequestBuilder()
   private implicit val unitPredicateHandler: PredicateHandler[String] = (x: RqResponse) => x.body.toString
 
   def getPlayResult(
-               rallyId: String
-             ): Future[Option[String]] = {
-        careteamRequestBuilder.getPlayResult(rallyId).execute()
+    rallyId: String
+  ): Future[Option[String]] = {
+    Rq.url("http://localhost:8018/rest/careteam/v1/primaryCare/" + rallyId)
+      .withHeaders(LegacyCookieGenerator.encodeSessionCookie(rallyId).toSeq: _*)
+      .withMethod(HttpVerbs.GET)
+      .withHandler(OkOrNotFound[String])
+      .execute()
+//        careteamRequestBuilder.getPlayResult(rallyId).execute()
   }
 }
 
@@ -86,18 +52,18 @@ import com.softwaremill.tagging.@@
 import play.api.http.HttpVerbs
 
 class CareteamRequestBuilder(
-  careteamRqClient: RqClient @@ CareteamClient
+//  careteamRqClient: RqClient @@ CareteamClient
 ) {
 
   private implicit val unitPredicateHandler: PredicateHandler[String] = (x: RqResponse) => x.body.toString
 
   def getPlayResult(
    rallyId: String
-  ): ExecutableRqRequest[Option[String]] =
+  ): HandledRqRequest[Option[String]] =
     Rq.url("http://localhost:8018/rest/careteam/v1/primaryCare/" + rallyId)
       .withHeaders(LegacyCookieGenerator.encodeSessionCookie(rallyId).toSeq: _*)
       .withMethod(HttpVerbs.GET)
       .withHandler(OkOrNotFound[String])
-      .withRqClient(careteamRqClient)
+//      .withRqClient(careteamRqClient)
 
 }
